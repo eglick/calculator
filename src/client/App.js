2c15f08a4c5cfecc9db7cad7893be16319b66de5
Ephraim Glick <eglick@prosper.com>
2019-03-21 14:30:15 -0700

    Set up repo structure

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+import ReactImage from './react.png';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>It works!</p>
+      </div>
+    );
+  }
+}
